d025553559a6d2f9e675370d9afacb7fe64bf7c4
Alexander Stavar <uberzerg9@gmail.com>
2022-12-05 22:29:26 +0200

    create server-side of project, create local API

---
+++ server/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: 'iPhone 12 PRO',
+        description: '6.1-inch display',
+        price: 999,
+        image: 'https://assets.swappie.com/cdn-cgi/image/width=600,height=600,fit=contain,format=auto/swappie-iphone-12-pro-silver.png?v=10'
+    },
+    {
+        id: 2,
+        name: 'iPhone 12',
+        description: '5.4-inch display',
+        price: 699,
+        image: 'https://costel.md/wp-content/uploads/2021/01/%D0%A1%D0%BC%D0%B0%D1%80%D1%82%D1%84%D0%BE%D0%BD-Apple-iPhone-12-64GB-White.-Chisinau.jpg'
+    },
+    {
+        id: 3,
+        name: 'Galaxy S',
+        description: '6.5-inch display',
+        price: 399,
+        image: 'https://images.samsung.com/is/image/samsung/p6pim/nz/sm-g990ezwgxnz/gallery/nz-galaxy-s21-fe-g990-424509-sm-g990ezwgxnz-thumb-534273833?$420_420_PNG$'
+    },
+]
+
+module.exports = products;
