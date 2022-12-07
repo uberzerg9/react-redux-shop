1e5de86eab478dc23106ea2414ef1309969d4148
Alexander Stavar <uberzerg9@gmail.com>
2022-12-07 22:05:47 +0200

    add redux toolkit, create products slicer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar(props) {
         </Link>
         <Link to="/cart">
             <div className="navbar__bag">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-bag-fill" viewBox="0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-bag-fill" viewBox="0">
                     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
                 </svg>
                 <span className="bag-quantity">
