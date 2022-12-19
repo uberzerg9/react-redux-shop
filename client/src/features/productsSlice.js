736ec8b226ef0167bd7d1c8efea28bb8e400a368
Alexander Stavar <uberzerg9@gmail.com>
2022-12-19 15:00:26 +0200

    add createAsyncThunk

--- client/src/features/productsSlice.js
+++ client/src/features/productsSlice.js
@@ -1,14 +1,36 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
     items: [],
-    status: null
-}
+    status: null,
+};
+
+export const productsFetch = createAsyncThunk(
+    "products/productsFetch",
+    async () => {
+        const res = await axios.get('http://localhost:5000/products')
+        return res?.data;
+    }
+)
 
 const productsSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [productsFetch.pending]: (state, action) => {
+            // immer
+            state.status = 'pending';
+        },
+        [productsFetch.fullfiled]: (state, action) => {
+            state.status = 'success';
+            state.items = action.payload;
+        },
+        [productsFetch.rejected]: (state, action) => {
+            state.status = 'rejected';
+        }
+    }
 })
 
 export default productsSlice.reducer;
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
 
-import productsReducer from './features/productsSlice';
+import productsReducer, {productsFetch} from './features/productsSlice';
 
 const store = configureStore({
   reducer: {
     products: productsReducer
   }
 })
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
